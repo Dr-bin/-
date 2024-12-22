3d9057c810ea4da4f42799c01fe814121a01f0fe
qiankun25 <161268848+qiankun25@users.noreply.github.com>
2024-12-22 17:41:27 +0800

    Add files via upload
    NPC系统

---
+++ Classes/NPCManager.cpp
@@ -0,0 +1,112 @@
+
+
+#include "NPCManager.h"
+
+
+USING_NS_CC;
+using namespace cocos2d;
+
+NPCManager* NPCManager::create() {
+    NPCManager* npcManager = new (std::nothrow) NPCManager();
+    if (npcManager && npcManager->init()) {
+        npcManager->autorelease();
+        return npcManager;
+    }
+    CC_SAFE_DELETE(npcManager);
+    return nullptr;
+}
+
+bool NPCManager::init() {
+    if (!Layer::init()) {
+        return false;
+    }
+
+    // Initialize event listeners
+    auto mouseListener = EventListenerMouse::create();
+    mouseListener->onMouseDown = CC_CALLBACK_1(NPCManager::onMouseDown, this);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(mouseListener, this);
+
+    //初始化放置
+
+    addNewNPC("Leah");
+    addNewNPC("Elliott");
+    addNewNPC("Alex");
+    addNewNPC("Amy");
+    return true;
+}
+
+void NPCManager::addNPC(NPC* npc) {
+    npcs.pushBack(npc);
+    this->addChild(npc);
+    npc->moveAround();
+}
+
+void NPCManager::removeNPC(NPC* npc) {
+    auto it = std::find(npcs.begin(), npcs.end(), npc);
+    if (it != npcs.end()) {
+        npcs.erase(it);
+        npc->stopAllActions();
+        this->removeChild(npc);
+    }
+}
+
+void NPCManager::onMouseDown(Event* event) {
+    EventMouse* e = (EventMouse*)event;
+    Vec2 clickLocation = Vec2(e->getCursorX(), e->getCursorY());
+
+    if (e->getMouseButton() == EventMouse::MouseButton::BUTTON_LEFT) {
+        // Left click event
+        checkForNPCs(clickLocation);
+    }
+    else if (e->getMouseButton() == EventMouse::MouseButton::BUTTON_RIGHT) {
+        // Right click event
+        //进入当前任务栏
+       // checkRightNPCs(clickLocation);
+
+    }
+}
+
+void NPCManager::checkForNPCs(Vec2 clickPosition) {
+    for (auto npc : npcs) {
+        if (npc->getCurrentSprite()->getBoundingBox().containsPoint(clickPosition)) {
+            npc->interact();
+            break;
+        }
+    }
+}
+
+void  NPCManager::addNewNPC(const std::string& npcName)
+{
+    Vec2 position = Vec2(xStart + (rand() % (xEnd - xStart)), yStart + (rand() % (yEnd - yStart)));
+    NPC* newNPC = nullptr;
+    if (npcName == "Amy")
+    {
+        newNPC = Amy::create(position);
+    }
+    else if (npcName == "Alex")
+    {
+        newNPC = Alex::create(position);
+    }
+    else if (npcName == "Leah")
+    {
+        newNPC = Leah::create(position);
+    }
+    else if (npcName == "Elliott")
+    {
+        newNPC = Elliott::create(position);
+    }
+    if (newNPC) {
+        addNPC(newNPC);
+    }
+
+}
+//void NPCManager::checkRightNPCs(Vec2 clickPosition) {
+//      for (auto npc : npcs) {
+//        if (npc->getCurrentSprite()->getBoundingBox().containsPoint(clickPosition)) {
+//            npc->turnTaskBar();
+//            auto taskbar = PopupBase::create();
+//            taskbar->show();
+//            break;
+//        }
+//    }
+//}
---
+++ Classes/NPCManager.h
@@ -0,0 +1,43 @@
+#ifndef NPC_MANAGER_H
+#define NPC_MANAGER_H
+
+#include "NPC.h"
+#include "cocos2d.h"
+#include "PopupBase.h"
+
+USING_NS_CC;
+
+class NPCManager : public Layer {
+private:
+    Vector<NPC*> npcs; // 存储所有的NPC
+    int xStart = 1100;
+    int xEnd = 1300;
+    int yStart = 200;
+    int yEnd = 450;
+
+public:
+    // 初始化事件监听
+    virtual bool init() override;
+
+    // 创建并返回一个 NPCManager 对象
+    static NPCManager* create();
+
+    // 添加NPC
+    void addNPC(NPC* npc);
+
+    // 删除NPC
+    void removeNPC(NPC* npc);
+
+    // 处理鼠标点击事件
+    void onMouseDown(Event* event);
+
+    // 检查点击位置是否有NPC
+    void checkForNPCs(Vec2 clickPosition);
+
+
+    // 创建并添加指定类型的NPC到场景中
+    void addNewNPC(const std::string& npcType);
+    //void  checkRightNPCs(clickLocation);
+};
+
+#endif // NPC_MANAGER_H
